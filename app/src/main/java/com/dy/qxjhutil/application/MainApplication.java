901a6a34ba5cb939bb977dc7c56b5896c1cfd33c
duyan <hbysduyan@yeah.net>
2019-08-16 17:14:30 +0800

    增加第三方悬浮窗工具库

--- app/src/main/java/com/dy/qxjhutil/application/MainApplication.java
+++ app/src/main/java/com/dy/qxjhutil/application/MainApplication.java
@@ -2,6 +2,7 @@ package com.dy.qxjhutil.application;
 
 import android.app.Application;
 
+import com.lzf.easyfloat.EasyFloat;
 import com.tencent.bugly.Bugly;
 import com.xuexiang.xui.XUI;
 
@@ -13,11 +14,13 @@ public class MainApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Bugly.init(getApplicationContext(), "06d0ae6d94", false);
+//        Bugly.init(getApplicationContext(), "e3d8b35bba", false);
 
         XUI.init(this); //初始化UI框架
         XUI.debug(true);  //开启UI框架调试日志
 
         Realm.init(this);
+        EasyFloat.init(this);
     }
 
 }
